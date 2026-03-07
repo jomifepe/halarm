6bb38fd15ad0b9c85d11e46282adb6594d4642b9
José Pereira <7235666+jomifepe@users.noreply.github.com>
2026-03-07 00:41:51 +0000

    Initial Commit

---
+++ halarm/halarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  halarmApp.swift
+//  halarm
+//
+//  Created by José Pereira on 07/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct halarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
